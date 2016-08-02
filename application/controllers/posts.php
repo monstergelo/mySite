30b8431e368ba4236280ea56718e4f89519ccbe9
sebastianusbara <sebastianus.bara@gmail.com>
2016-08-02 23:39:43 +0700

    fix pagination

--- application/controllers/posts.php
+++ application/controllers/posts.php
@@ -4,17 +4,25 @@ class Posts extends CI_Controller
 {
 	function index($start=0)
 	{
+		$limit = 2;
 		$this->load->model('post');
-
 		$this->load->library('pagination');
-		$config['base_url'] = 'http://[::1]/homepage//index.php/posts/';
-		$config['total_rows'] = $this->post->post_count();
-		$config['per_page'] = 1;
-		$this->pagination->initialize($config);
-		$data['pages'] = $this->pagination->create_links();
+		$data['posts']=$this->post->get_posts($limit, $start);
+		// $config['base_url'] = 'http://localhost/mySite/index.php/posts/';
+		// $config['total_rows'] = $this->post->post_count();
+		// $config['uri_segment'] = 3;
+		// $config['per_page'] = 1;
 		
-		$data['posts']=$this->post->get_posts(1, $start);
+		$config['base_url']     =   'http://localhost/mySite/index.php/posts/index';
+		$config['total_rows']   =   $this->post->post_count();;
+		$config['per_page']     =   $limit;
+		$config['uri_segment']  =   3;
+		$this->pagination->initialize($config);
+
+		$data['pages'] = $this->pagination->create_links();
 		$this->load->view('blog.php', $data);
+
+
 	}
 }
 
